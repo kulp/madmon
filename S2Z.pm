599d826dd0c2ecc7dfefd4b0112d8f7134dec2aa
Darren Kulp <darren@kulp.ch>
2010-08-07 19:22:50 -0500

    WIP. Starting to do things.

--- S2Z.pm
+++ S2Z.pm
@@ -6,10 +6,20 @@ use 5.10.0;
 use Archive::Zip qw(:ERROR_CODES :CONSTANTS);
 use XML::Simple;
 
+# TODO use my own naming / versioning ?
+my $default_comment = <<EOM;
+HoN Mod Manager v1.3.6.0 Output
+
+Game Version: 1.0.6.1
+
+Applied Mods: 
+EOM
+
 sub new
 {
     my $class = shift;
-    return bless { @_ } => $class;
+    my (%args) = @_;
+    return bless \%args => $class;
 }
 
 sub read
@@ -18,7 +28,13 @@ sub read
 
     my $zip = $self->{zip} = Archive::Zip->new;
 
-    $zip->read($self->{filename}) == AZ_OK or die "Failed to read ZIP: $!";
+    if (!-e $self->{filename} and $self->{create}) {
+        $zip->zipfileComment($default_comment);
+        $zip->overwriteAs($self->{filename});
+    }
+
+    $zip->read($self->{filename}) == AZ_OK
+        or die "Failed to read ZIP: $!";
 }
 
 sub file
@@ -31,7 +47,15 @@ sub file
 sub comment
 {
     my $self = shift;
-    eval { return $self->{zip}->zipfileComment; };
+    return $self->{zip}->zipfileComment(@_);
+}
+
+sub save
+{
+    # TODO permit writing to another filename
+    my $self = shift;
+    $self->{zip}->overwrite == AZ_OK
+        or die "Failed to write file";
 }
 
 package HoN::S2Z::Honmod;
@@ -53,24 +77,58 @@ sub parse
         or die "Bad format";
     my $gversion = $lines[2] =~ /Game Version: (\S+)/
         or die "No game version";
-    my %applieds = map {
+    my @applieds = map {
         my ($name, $version) = /^(.*)\s+\(v(.*)\)$/;
-        $name => {
+        $name ? () :
+        {
             name    => $name,
             version => $version,
         }
-    } @lines[(firstidx { /Applied Mods: / } @lines) .. $#lines];
+    } @lines[(1 + firstidx { /Applied Mods: / } @lines) .. $#lines];
 
     %$self = ( %$self,
         game_version => $gversion,
         mod_version  => $mversion,
-        applied_mods => \%applieds,
+        applied_mods => \@applieds,
     );
 }
 
 sub mods
 {
     return shift->{applied_mods};
+}
+
+sub have
+{
+    my ($self, $name) = @_;
+    my %mods = map { $_->{name} => $_ } @{ $self->{applied_mods} };
+    return $mods{$name};
+}
+
+sub installed
+{
+    my ($self, $mod, $ver) = @_;
+    if ($mod) {
+        push @{ $self->{applied_mods} }, +{
+            name    => $mod,
+            version => $ver,
+        };
+    }
+
+    # TODO wantarray
+    return @{ $self->{applied_mods} };
+}
+
+sub save
+{
+    my $self = shift;
+
+    my $built = join "",
+        map "$_->{name} (v$_->{version})\n",
+        @{ $self->{applied_mods} };
+
+    $self->comment($default_comment . $built);
+    return $self->SUPER::save(@_);
 }
 
 1;
--- honmodman.pl
+++ honmodman.pl
@@ -12,6 +12,9 @@ my $gamedir = "."; # XXX
 my $resfilename = "resources999.s2z";
 my $resfilepath = "$gamedir/resources999.s2z";
 my ($applydir) = @ARGV;
+my $repores = HoN::S2Z::Honmod->new(create => 1, filename => $resfilepath);
+$repores->read;
+$repores->parse;
 
 for my $modfilename (glob "$applydir/*.honmod") {
     apply_mod($modfilename);
@@ -20,35 +23,71 @@ for my $modfilename (glob "$applydir/*.honmod") {
 sub apply_mod
 {
     my ($modfilename) = @_;
-    my $mod = HoN::Honmod->new(filename => $modfilename);
-    my $modxml = $mod->read;
+    my $modres = HoN::Honmod->new(filename => $modfilename);
+    my $modxml = $modres->read;
 
     my @resfiles = sort {
         ($a =~ /(\d+)/)[0] <=> ($b =~ /(\d+)/)[0]
     } grep !/$resfilename$/o, glob "$gamedir/resources*.s2z";
     my @res = map { HoN::S2Z->new(filename => $_) } @resfiles;
     my $prires = $res[0];
-    my $modres = HoN::S2Z::Honmod->new(filename => $resfilepath);
-    $modres->read;
-    $modres->parse;
+
+    for my $req (keys %{ $modxml->{requirement} }) {
+        if (not $repores->have($req)) {
+            # TODO upgrade to die()
+            warn "Missing requirement $req";
+        }
+    }
+
+    while (my ($filename, $copy) = each %{ $modxml->{copyfile} }) {
+        say "Copying $filename into repo ...";
+        my $member = $modres->{zip}->memberNamed($copy->{source});
+        # TODO don't use stringification, it's probably inefficient
+        my $newmember = Archive::Zip::Member->newFromString(scalar $member->contents, $filename);
+        $repores->{zip}->removeMember($filename);
+        $repores->{zip}->addMember($newmember);
+    }
 
     while (my ($filename, $edits) = each %{ $modxml->{editfile} }) {
         say "$filename has edits";
         # search backward to find the last resources file that includes the file
         # we need to change
         SEARCH_RES:
-        for my $res ($modres, reverse @res) {
+        for my $res (reverse @res) {
             $res->read;
             if (my $file = $res->file($filename)) {
                 say "res $res->{filename} has $filename";
 
-                # TODO apply mods
+                # TODO encapsulation
+                my $zip = $repores->{zip};
+                my $str = $file->contents;
+
+                do_edit($str, $edits);
+
+                # TODO apply mods to $str
+                my $member = Archive::Zip::Member->newFromString($str, $filename);
+                $zip->removeMember($filename);
+                $zip->addMember($member);
 
                 last SEARCH_RES;
             }
         }
     }
+
+    my $modname = $modxml->{name};
+    my $modversion = $modxml->{mmversion};
+
+    $repores->installed($modname, $modversion);
 }
+
+sub do_edit
+{
+    my ($str, $edits) = @_;
+    warn "not editing ".\$str." with $edits";
+}
+
+$repores->save
+    or die "Failed to write repo";
 
 #XXX $modxml;
 #XXX $res->mods;
