29393678744cf8430b88abe08c904440761ec602
Darren Kulp <darren@kulp.ch>
2010-08-13 18:13:23 -0500

    Refactoring

--- honmodman.pl
+++ honmodman.pl
@@ -27,12 +27,23 @@ my @mods = glob "$applydir/*.honmod";
 my @modres = map { HoN::Honmod->new(filename => $_) } @mods;
 $_->read for @modres;
 
+my %orignames;
+my %normnames;
+
 my %deps;
 for my $m (@modres) {
     my $xml = $m->xml;
-    my @reqs = grep { %$_ } map { $_->pointer } @{ $xml->{requirement} };
-    $deps{ $xml->{name} } = {
-        name => $xml->{name}->pointer,
+    my @reqs =
+        map { $_->{name} = fix_mod_name($_->{name}); $_ }
+        grep { %$_ }
+        map { $_->pointer }
+        @{ $xml->{requirement} };
+    my $origname = $xml->{name}->pointer;
+    my $name = fix_mod_name($origname);
+    $orignames{$name} = $origname;
+    $normnames{$origname} = $name;
+    $deps{ $name } = {
+        name => $name,
         # TODO version
         reqs => \@reqs,
         mod  => $m,
@@ -44,43 +55,34 @@ my %deptree = map {
 } sort { +@{ $a->{reqs} } - +@{ $b->{reqs} } } values %deps;
 
 my $depsrc = Algorithm::Dependency::Source::HoA->new(\%deptree);
-my $depmkr = Algorithm::Dependency::Ordered->new(source => $depsrc) # TODO ignore_orphans
+my $depmkr = Algorithm::Dependency::Ordered->new(source => $depsrc, ignore_orphans => 1) # TODO ignore_orphans
     or die "Failed to create dependency tree resolver";
 
-my @ordered = map { $deps{$_}->{mod} } @{ $depmkr->schedule_all };
+my $sched = $depmkr->schedule_all
+    or die "Failed to find a schedule to enable all mods";
+print "Going to apply mods:\n", map { "\t$orignames{$_}\n" } @$sched;
+my @ordered = map { $deps{$_}->{mod} } @$sched;
 
 for my $modres (@ordered) {
-    # TODO retry logic
-    say "Trying to apply mod " . $modres->{filename} . " ...";
+    say "Applying mod " . $modres->{filename} . " ...";
     apply_mod($modres);
 }
 
-sub apply_mod
+# this is from the original HoN Modification Manager
+# this normalizes mod names and hides at least one known mistake in reqspec
+# (Bang! Replay Renamer 0.52)
+sub fix_mod_name
+{
+    (my $x = lc $_[0]) =~ y/a-z0-9//dc;
+    return $x;
+}
+
+sub do_copies
 {
     my ($modres) = @_;
     my $modxml = $modres->xml;
 
-    my $modname    = $modxml->{name};
-    my $modversion = $modxml->{mmversion};
-
-    my @resfiles = sort {
-        ($a =~ /(\d+)/)[0] <=> ($b =~ /(\d+)/)[0]
-    } grep !/$resfilename$/o, glob "$gamedir/resources*.s2z";
-    my @res = map { HoN::S2Z->new(filename => $_) } @resfiles;
-    die "Base resources file (resources0.s2z) not found"
-        unless $res[0]->{filename} ne "resources0.s2z";
-
-    for my $req (values %{ $modxml->{requirement}->pointer }) {
-        die "Missing requirement $req"
-            unless $repores->have($req);
-    }
-
-    for my $bad (values %{ $modxml->{incompatibility}->pointer }) {
-        die "Present incompatibility $bad"
-            if $repores->have($bad);
-    }
-
-    for my $copy (@{ $modxml->{copyfile} }) {
+    for my $copy (grep { %$_ } @{ $modxml->{copyfile} }) {
         my $filename = $copy->{name};
         my $source = $copy->{source} || $filename;
         say "Copying $filename into repo ...";
@@ -91,18 +93,20 @@ sub apply_mod
         $repores->{zip}->removeMember($filename);
         $repores->{zip}->addMember($newmember);
     }
+}
 
-    # TODO read only on demand
-    $_->read for @res;
+sub do_edits
+{
+    my ($modres, $res) = @_;
+    my $modxml = $modres->xml;
 
     EDIT:
     for my $edits (@{ $modxml->{editfile} }) {
-        #XXX $edits->pointer;
         my $filename = "$edits->{name}"; # force stringification of XML::Smart object
         say "Editing $filename ...";
         # search backward to find the last resources file that includes the file
         # we need to change
-        for my $res ($repores, reverse @res) {
+        for my $res ($repores, reverse @$res) {
             if (my $file = $res->file($filename)) {
                 say "... found $filename in $res->{filename} ...";
 
@@ -122,8 +126,44 @@ sub apply_mod
             }
         }
     }
+}
 
-    $repores->installed($modname, $modversion);
+sub apply_mod
+{
+    my ($modres) = @_;
+    my $modxml = $modres->xml;
+
+    my $modname    = $modxml->{name};
+    my $modversion = $modxml->{mmversion};
+
+    my @resfiles = sort {
+        ($a =~ /(\d+)/)[0] <=> ($b =~ /(\d+)/)[0]
+    } grep !/$resfilename$/o, glob "$gamedir/resources*.s2z";
+    my @res = map { HoN::S2Z->new(filename => $_) } @resfiles;
+    die "Base resources file (resources0.s2z) not found"
+        unless $res[0]->{filename} ne "resources0.s2z";
+
+    for my $req (grep { %$_ } @{ $modxml->{requirement} }) {
+        die "Missing requirement $req->{name}"
+            unless $repores->have($req->{name});
+    }
+
+    for my $bad (grep { %$_ } @{ $modxml->{incompatibility} }) {
+        die "Present incompatibility $bad->{name}"
+            if $repores->have($bad->{name});
+    }
+
+    eval {
+        do_copies($modres);
+        $_->read for @res; # TODO read only on demand
+        do_edits($modres, \@res);
+    };
+    if ($@) {
+        warn "Caught error while applying mod: $@";
+        die "Application of mod $modname $modversion failed; state is undefined";
+    }
+
+    $repores->installed($normnames{$modname}, $modversion);
 }
 
 sub nodos
@@ -136,10 +176,13 @@ sub do_edit
 {
     my ($str, $edit) = @_;
 
-    # XXX use condition
-    my $condition;
     my $pos = 0;
     my $len = 0;
+
+    # XXX use condition
+    my $condition = sub {
+        die "Conditions not implemented; unsafe to continue";
+    };
 
     my $find = sub {
         my ($what, $up) = @_;
@@ -191,7 +234,7 @@ sub do_edit
     for (@{ $edit->pointer->{"/order"} }) {
         my $what = ${ $edit->{$_} }[ $ctr{$_}++ ]->pointer;
         /(find|search|seek)(up)?/ and $find->($what, defined $2);
-        /condition/               and $condition = $what;
+        /condition/               and $condition->($what);
         /insert|add/              and $insert->($what);
         /delete/                  and $delete->($what);
         /replace/                 and $replace->($what);
